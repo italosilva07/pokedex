62754e302ea92c866ec9e1caa1e519909929dfa7
Italo Silva <italoacademy@gmail.com>
2022-11-21 13:46:25 -0300

    Adicionando poke-api e separando a manipulação do html de main.js para poke-api

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-
-const offset = 0 ;
-const limit =10 ;
+const offset = 0
+const limit = 10
 
 const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
+
 
 function convertPokemonToLi(pokemon) {
     return `<li class="pokemon">
@@ -31,18 +32,18 @@ const pokemonList = document.getElementById('pokemonList')
 
 
 
-fetch(url)
-.then((response) => response.json())
-.then((jsonBody) => jsonBody.results)
-.then ((pokemons) => {
-     
+pokeApi.getPokemons().then ((pokemons) => {
+     const listItens = [] 
     for (let i = 0; i < pokemons.length; i++) {
+        
+        
         const pokemon = pokemons[i];
-        pokemonList.innerHTML += convertPokemonToLi(pokemon)
+        listItens.push(convertPokemonToLi(pokemon))
 
       
         
     }
+        console.log(listItens);
     
 })
 .catch((error) => console.log(error))
